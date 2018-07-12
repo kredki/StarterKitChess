d5a4d1c08e403bab67767d9409aabfaaf1b197c4
TL <31960565+kredki@users.noreply.github.com>
2018-07-12 14:12:14 +0200

    Validator class

---
+++ ChessAlgorithms/src/main/java/com/capgemini/chess/algorithms/implementation/Validator.java
@@ -0,0 +1,20 @@
+package com.capgemini.chess.algorithms.implementation;
+
+import com.capgemini.chess.algorithms.data.Coordinate;
+import com.capgemini.chess.algorithms.data.enums.Color;
+import com.capgemini.chess.algorithms.data.generated.Board;
+
+public class Validator {
+	private Coordinate from;
+	private Coordinate to;
+	private Board board;
+	private Color actualPlayerColor;
+	
+	public Validator(Coordinate from, Coordinate to, Board board, Color actualPlayerColor) {
+		super();
+		this.from = from;
+		this.to = to;
+		this.board = board;
+		this.actualPlayerColor = actualPlayerColor;
+	}
+}

ba07829de684fbe4dc9cf8da5d22a4c19350108f
TL <31960565+kredki@users.noreply.github.com>
2018-07-17 14:15:08 +0200

    rook validator return move + tests

--- ChessAlgorithms/src/main/java/com/capgemini/chess/algorithms/implementation/validators/RookValidator.java
+++ ChessAlgorithms/src/main/java/com/capgemini/chess/algorithms/implementation/validators/RookValidator.java
@@ -3,6 +3,8 @@ package com.capgemini.chess.algorithms.implementation.validators;
 import com.capgemini.chess.algorithms.data.Coordinate;
 import com.capgemini.chess.algorithms.data.Move;
 import com.capgemini.chess.algorithms.data.enums.Color;
+import com.capgemini.chess.algorithms.data.enums.MoveType;
+import com.capgemini.chess.algorithms.data.enums.Piece;
 import com.capgemini.chess.algorithms.data.generated.Board;
 import com.capgemini.chess.algorithms.implementation.Validator;
 import com.capgemini.chess.algorithms.implementation.exceptions.InvalidMoveException;
@@ -26,7 +28,12 @@ public class RookValidator implements Validator {
 		int deltaX = Math.abs(this.to.getX() - this.from.getX());
 		int deltaY = Math.abs(this.to.getY() - this.from.getY());
 		if ((deltaX == 0 && deltaY > 0) || (deltaX > 0 && deltaY == 0)) {
-			return null;
+			Piece pieceAtTo = board.getPieceAt(to);
+			if(pieceAtTo != null) {
+				return new Move(from, to, MoveType.CAPTURE, board.getPieceAt(from));
+			} else {
+				return new Move(from, to, MoveType.ATTACK, board.getPieceAt(from));
+			}
 		} else {
 			throw new InvalidMoveException();
 		}
--- ChessAlgorithms/src/test/java/com/capgemini/chess/algorithms/implementation/RookValidatorTest.java
+++ ChessAlgorithms/src/test/java/com/capgemini/chess/algorithms/implementation/RookValidatorTest.java
@@ -1,12 +1,18 @@
 package com.capgemini.chess.algorithms.implementation;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 import com.capgemini.chess.algorithms.data.Coordinate;
+import com.capgemini.chess.algorithms.data.Move;
 import com.capgemini.chess.algorithms.data.enums.Color;
+import com.capgemini.chess.algorithms.data.enums.MoveType;
 import com.capgemini.chess.algorithms.data.enums.Piece;
+import com.capgemini.chess.algorithms.data.enums.PieceType;
 import com.capgemini.chess.algorithms.data.generated.Board;
 import com.capgemini.chess.algorithms.implementation.exceptions.InvalidMoveException;
+import com.capgemini.chess.algorithms.implementation.validators.QueenValidator;
 import com.capgemini.chess.algorithms.implementation.validators.RookValidator;
 
 public class RookValidatorTest {
@@ -38,5 +44,44 @@ public class RookValidatorTest {
 
 		// then
 		validator.validate();
+	}
+	
+	@Test
+	public void shouldReturnAttack() throws InvalidMoveException {
+		// given
+		Board board = new Board();
+		board.setPieceAt(Piece.WHITE_ROOK, new Coordinate(4, 4));
+		Coordinate from = new Coordinate(4, 4);
+		Coordinate to = new Coordinate(4, 2);
+		
+		//when
+		Validator validator = new QueenValidator(from, to, board, Color.WHITE);
+		Move move = validator.validate();
+		
+		//then
+		assertEquals(MoveType.ATTACK, move.getType());
+		assertEquals(from, move.getFrom());
+		assertEquals(to, move.getTo());
+		assertEquals(PieceType.ROOK, move.getMovedPiece().getType());
+	}
+	
+	@Test
+	public void shouldReturnCapture() throws InvalidMoveException {
+		// given
+		Board board = new Board();
+		board.setPieceAt(Piece.WHITE_ROOK, new Coordinate(4, 4));
+		board.setPieceAt(Piece.BLACK_PAWN, new Coordinate(4, 2));
+		Coordinate from = new Coordinate(4, 4);
+		Coordinate to = new Coordinate(4, 2);
+		
+		//when
+		Validator validator = new QueenValidator(from, to, board, Color.WHITE);
+		Move move = validator.validate();
+		
+		//then
+		assertEquals(MoveType.CAPTURE, move.getType());
+		assertEquals(from, move.getFrom());
+		assertEquals(to, move.getTo());
+		assertEquals(PieceType.ROOK, move.getMovedPiece().getType());
 	}
 }

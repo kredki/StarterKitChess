31d09ad3e81eb1985a58cdfb3d5230a34d6367c8
TL <31960565+kredki@users.noreply.github.com>
2018-07-17 13:43:56 +0200

    king validator return move + tests

--- ChessAlgorithms/src/main/java/com/capgemini/chess/algorithms/implementation/validators/KingValidator.java
+++ ChessAlgorithms/src/main/java/com/capgemini/chess/algorithms/implementation/validators/KingValidator.java
@@ -3,6 +3,8 @@ package com.capgemini.chess.algorithms.implementation.validators;
 import com.capgemini.chess.algorithms.data.Coordinate;
 import com.capgemini.chess.algorithms.data.Move;
 import com.capgemini.chess.algorithms.data.enums.Color;
+import com.capgemini.chess.algorithms.data.enums.MoveType;
+import com.capgemini.chess.algorithms.data.enums.Piece;
 import com.capgemini.chess.algorithms.data.enums.PieceType;
 import com.capgemini.chess.algorithms.data.generated.Board;
 import com.capgemini.chess.algorithms.implementation.Validator;
@@ -27,7 +29,12 @@ public class KingValidator implements Validator {
 		int deltaX = Math.abs(this.to.getX() - this.from.getX());
 		int deltaY = Math.abs(this.to.getY() - this.from.getY());
 		if (deltaX < 2 && deltaY < 2) {
-			return null;
+			Piece pieceAtTo = board.getPieceAt(to);
+			if(pieceAtTo != null) {
+				return new Move(from, to, MoveType.CAPTURE, board.getPieceAt(from));
+			} else {
+				return new Move(from, to, MoveType.ATTACK, board.getPieceAt(from));
+			}
 		} else {
 			throw new InvalidMoveException();
 		}
--- ChessAlgorithms/src/test/java/com/capgemini/chess/algorithms/implementation/KingValidatorTest.java
+++ ChessAlgorithms/src/test/java/com/capgemini/chess/algorithms/implementation/KingValidatorTest.java
@@ -1,12 +1,18 @@
 package com.capgemini.chess.algorithms.implementation;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 import com.capgemini.chess.algorithms.data.Coordinate;
+import com.capgemini.chess.algorithms.data.Move;
 import com.capgemini.chess.algorithms.data.enums.Color;
+import com.capgemini.chess.algorithms.data.enums.MoveType;
 import com.capgemini.chess.algorithms.data.enums.Piece;
+import com.capgemini.chess.algorithms.data.enums.PieceType;
 import com.capgemini.chess.algorithms.data.generated.Board;
 import com.capgemini.chess.algorithms.implementation.exceptions.InvalidMoveException;
+import com.capgemini.chess.algorithms.implementation.validators.BishopValidator;
 import com.capgemini.chess.algorithms.implementation.validators.KingValidator;
 
 public class KingValidatorTest {
@@ -38,5 +44,44 @@ public class KingValidatorTest {
 
 		// then
 		validator.validate();
+	}
+	
+	@Test
+	public void shouldReturnAttack() throws InvalidMoveException {
+		// given
+		Board board = new Board();
+		board.setPieceAt(Piece.WHITE_KING, new Coordinate(4, 4));
+		Coordinate from = new Coordinate(4, 4);
+		Coordinate to = new Coordinate(5, 5);
+		
+		//when
+		Validator validator = new KingValidator(from, to, board, Color.WHITE);
+		Move move = validator.validate();
+		
+		//then
+		assertEquals(MoveType.ATTACK, move.getType());
+		assertEquals(from, move.getFrom());
+		assertEquals(to, move.getTo());
+		assertEquals(PieceType.KING, move.getMovedPiece().getType());
+	}
+	
+	@Test
+	public void shouldReturnCapture() throws InvalidMoveException {
+		// given
+		Board board = new Board();
+		board.setPieceAt(Piece.WHITE_KING, new Coordinate(4, 4));
+		board.setPieceAt(Piece.BLACK_ROOK, new Coordinate(5, 5));
+		Coordinate from = new Coordinate(4, 4);
+		Coordinate to = new Coordinate(5, 5);
+		
+		//when
+		Validator validator = new KingValidator(from, to, board, Color.WHITE);
+		Move move = validator.validate();
+		
+		//then
+		assertEquals(MoveType.CAPTURE, move.getType());
+		assertEquals(from, move.getFrom());
+		assertEquals(to, move.getTo());
+		assertEquals(PieceType.KING, move.getMovedPiece().getType());
 	}
 }
